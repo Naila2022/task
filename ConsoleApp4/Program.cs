365b62c1e19200f5dace42ed627b980097735835
Sevil Alovsadli <nailaib@code.edu.az>
2022-04-20 13:56:44 +0400

    Task1

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir Reqem daxil edin ");
+            string n = Console.ReadLine();
+            int i=0;
+            int count = n.Length;
+            int sum = 0;
+            while (i < count)
+            {
+                if (char.IsDigit(n[i]))
+                    sum += n[i++] - 48;
+            }
+            Console.WriteLine(sum);
+
+
+       
+        }
+    }
+}
